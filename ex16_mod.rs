c2f3dfcdf0b967ec4dbd95fdd91caa0db50fab81
Ricardo Silva <ricardo7c@hotmail.com>
2024-10-19 00:18:19 -0300

    Finalizado!

---
+++ ex16_mod.rs
@@ -0,0 +1,16 @@
+pub fn soma(a:i32, b:i32) -> i32{
+    let x = a+b;
+    x
+}
+pub fn sub(a:i32, b:i32) -> i32{
+    let x = a-b;
+    x
+}
+pub fn div(a:i32, b:i32) -> i32{
+    let x = a/b;
+    x
+}
+pub fn mult(a:i32, b:i32) -> i32{
+    let x = a*b;
+    x
+}
